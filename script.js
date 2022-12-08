a75feca6326e696587e7b51891bbe378a2064abc
Asrul Haque <asrulhaque123@gmail.com>
2022-12-08 20:20:13 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const toggle = document.querySelector('.toggle')
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
+
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+// const toggle = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function setTime1() {
+  const time = new Date();
+  const month = time.getMonth()
+  const day = time.getDay()
+  const date = time.getDate()
+  const hours = time.getHours()
+  const hoursForClock = hours >= 13 ? hours % 12 : hours;
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+  const ampm = hours >= 12 ? 'PM' : 'AM'
+
+  // hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+  // minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+  // secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+
+  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+setTime1()
+// setInterval(setTime, 1000)
